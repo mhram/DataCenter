a429de0128b422ef5821554fd00302109960cebf
mhram <ramkhoo@yahoo.com>
2016-12-28 01:49:02 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+
+	public static void main(String[] args) {
+	Server HP=new Server();
+	Server IBM=new Server();
+	Server Intel=new Server();
+	
+	IBM.type= "Web Server";
+	HP.type= "Printer Server";
+	Intel.type= "Database Server";
+	
+
+}
+}
